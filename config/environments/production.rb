9837a8a11081de80dc141b520b0e259d554d3f74
Senior Project <TAMUKSeniorProject@gmail.com>
2018-02-06 15:57:47 +0000

    Installed Thredded gem and added user email confirmation during sign up process

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,14 @@ Rails.application.configure do
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => "127.0.0.1",
+  :port    => 25,
+  :domain  => 'example.com'
+}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+  
+  
 end
