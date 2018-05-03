616e844f9190546fd1dee920db1daed08a5d0831
Senior Project <TAMUKSeniorProject@gmail.com>
2018-05-03 21:41:25 +0000

    Added links in the header to the calendar and moderation views

--- app/controllers/moderation_controller.rb
+++ app/controllers/moderation_controller.rb
@@ -1,5 +1,6 @@
 class ModerationController < ApplicationController
     def index
+        redirect_to(root_url) unless current_user.has_role?(:admin)
         @discussions = Discussion.joins(:user).where(users: {first_post_approved: false}).order('discussions.created_at desc')
         #@user = User.find(current_user.id)
     end
